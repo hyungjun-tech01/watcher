dc338e1a7a31567b02e31079e3fada47ffa292b6
hyungjun-tech01 <whmoon00@naver.com>
2024-06-04 21:08:38 +0900

    pdf viewer 추가

--- client/src/components/AuditLogView/AuditLogPdfViewer.tsx
+++ client/src/components/AuditLogView/AuditLogPdfViewer.tsx
@@ -1,33 +1,58 @@
-import React, { useEffect } from 'react';
+import React, { useState } from 'react';
+import { PDFDocumentProxy } from 'pdfjs-dist/types/src/display/api';
+import 'react-pdf/dist/esm/Page/AnnotationLayer.css';
+import 'react-pdf/dist/esm/Page/TextLayer.css';
+import { Document, Page, pdfjs } from 'react-pdf';
 
+pdfjs.GlobalWorkerOptions.workerSrc = `/pdf.worker.min.mjs`;
 
 interface IAuditLogPdfViewer {
     pdfUrl: string | undefined,
     onClose : (value:boolean) => void,
   };
 const AuditLogPdfViewer = ({pdfUrl, onClose}: IAuditLogPdfViewer) => {
-  useEffect(() => {
-    const handleContextMenu = (e:any) => {
-      e.preventDefault();
-    };
+  const [numPages, setNumPages] = useState<number|null>(null);
 
-    document.addEventListener('contextmenu', handleContextMenu);
+  const onDocumentLoadSuccess = (pdf: PDFDocumentProxy) => {
+    setNumPages(pdf.numPages);
+  };
 
-    // Cleanup the event listener on component unmount
-    return () => {
-      document.removeEventListener('contextmenu', handleContextMenu);
-    };
-  }, []);
+  console.log(pdfUrl);
+  return(
+    <div style={{ height: '75vh', overflow: 'auto' }}>
+    <Document
+        file={pdfUrl}
+        onLoadSuccess={onDocumentLoadSuccess}
+        renderMode="canvas"
+      >
+        {Array.from(new Array(numPages), (el, index) => (
+          <Page key={`page_${index + 1}`} pageNumber={index + 1} />
+        ))}
+      </Document>
+  </div>
+  )
+  // useEffect(() => {
+  //   const handleContextMenu = (e:any) => {
+  //     e.preventDefault();
+  //   };
 
-  return (
-    <div>
-      <iframe
-        title="pdfviewer"
-        src={pdfUrl}
-        style={{ width: '800px', height: '75vh', border: 'none' }}
-      ></iframe>
-    </div>
-  );
+  //   document.addEventListener('contextmenu', handleContextMenu);
+
+  //   // Cleanup the event listener on component unmount
+  //   return () => {
+  //     document.removeEventListener('contextmenu', handleContextMenu);
+  //   };
+  // }, []);
+
+  // return (
+  //   <div>
+  //     <iframe
+  //       title="pdfviewer"
+  //       src={pdfUrl}
+  //       style={{ width: '800px', height: '75vh', border: 'none' }}
+  //     ></iframe>
+  //   </div>
+  // );
 };
 
 export default AuditLogPdfViewer;
